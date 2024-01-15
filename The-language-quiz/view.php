942d940e43a065b1c3e35d9a519f8ba1e4c8a9c1
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2024-01-15 11:11:43 +0100

    Create view area componets for game

--- The-language-quiz/view.php
+++ The-language-quiz/view.php
@@ -6,8 +6,22 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Game</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- TODO: add a form for the user to play the game -->
+    <div class="container">
+    <h1>Wordtranslation game</h1>
+    <h2>Translate this French word and earn points</h2>
+    <div id="gameArea">
+    <section id="prompt">
+
+</section>
+<section id="answer">
+    <input type="text">
+</section>
+    </div>
+ 
+    </div>
+
 </body>
 </html>
